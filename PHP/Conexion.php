7f5e99bd33195dcf23f776b3b8e74f39d559857a
DaniHS <danielhachac@gmail.com>
2019-05-27 15:12:13 -0500

    1.6.1

---
+++ PHP/Conexion.php
@@ -0,0 +1,11 @@
+<?php
+  $db_host="localhost";
+  $db_name="id9433736_elefanteform";
+  $db_login="id9433736_root";
+  $db_pswd="Lucky258";
+  $con=mysqli_connect($db_host,$db_login,$db_pswd,$db_name);
+  if($con){
+    //echo "bien_Conexion";
+  }
+  else{ echo "error_Conexion";}
+?>
